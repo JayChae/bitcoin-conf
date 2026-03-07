7afcfde7fc341b92b433ffd1295e84b9fca236d2
creditcityIvar <po.chajm@creditncity.com>
2026-03-07 19:50:15 +0900

    스폰서

--- app/[locale]/(2026)/page.tsx
+++ app/[locale]/(2026)/page.tsx
@@ -1,6 +1,8 @@
 import { getTranslations } from "next-intl/server";
 import Hero from "./_components/Hero";
 import ComingSoon from "./_components/ComingSoon";
+import Section from "./_components/Section";
+import Sponsor from "./_components/Sponsor";
 
 type Props = {
   params: Promise<{ locale: string }>;
@@ -9,6 +11,7 @@ type Props = {
 export default async function Home2026({ params }: Props) {
   const { locale } = await params;
   const tHero = await getTranslations("Hero2026");
+  const tSponsor = await getTranslations("Sponsor");
 
   return (
     <main className="">
@@ -19,6 +22,15 @@ export default async function Home2026({ params }: Props) {
         date={tHero("date")}
       />
       <ComingSoon />
+      <Section id="sponsors" title={tSponsor("title")}>
+        <Sponsor
+          diamondTitle={tSponsor("diamond")}
+          goldTitle={tSponsor("gold")}
+          silverTitle={tSponsor("silver")}
+          bronzeTitle={tSponsor("bronze")}
+          comingSoonText={tSponsor("comingSoon")}
+        />
+      </Section>
     </main>
   );
 }
